3cf6f664c0a0d8efd8604eab5a22b231fa0c46d2
Leonardo K. R. <leo@ziondev.us>
2026-02-06 15:24:01 -0300

    Update package init with new exports and bump version to 3.0.0

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,35 @@
 """
-Image Extractor Package
+eBook Image Extractor Package
 
-A Python tool to extract images from files with advanced filtering and organization features.
+A Python tool to extract images from EPUB and MOBI/AZW files with advanced
+filtering, deduplication, and organization features.
 """
 
+from .base_extractor import BaseExtractor, ExtractionStats, BookMetadata, ImageInfo
 from .epub_extractor import EPUBImageExtractor
 from .mobi_extractor import MobiImageExtractor
+from .exceptions import (
+    EbookExtractorError,
+    InvalidFileError,
+    ExtractionError,
+    CorruptedFileError,
+    NoImagesFoundError,
+    OutputDirectoryError,
+)
 
-__all__ = ["EPUBImageExtractor", "MobiImageExtractor"]
+__version__ = "3.0.0"
+
+__all__ = [
+    "BaseExtractor",
+    "EPUBImageExtractor",
+    "MobiImageExtractor",
+    "ExtractionStats",
+    "BookMetadata",
+    "ImageInfo",
+    "EbookExtractorError",
+    "InvalidFileError",
+    "ExtractionError",
+    "CorruptedFileError",
+    "NoImagesFoundError",
+    "OutputDirectoryError",
+]
